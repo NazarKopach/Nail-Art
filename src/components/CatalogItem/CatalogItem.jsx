d451d68bb1744eeff9c9d761ca39444d07625864
NazarKopach <164062094+NazarKopach@users.noreply.github.com>
2025-08-09 19:29:28 +0200

    change color

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -25,58 +25,71 @@ const CatalogItem = () => {
 
   return (
     <div className={styles.catalog_item_div}>
-      <ul>
+      <ul className={styles.catalog_item_list}>
         {Array.isArray(bookings) && bookings.length > 0 ? (
           bookings.map((booking) => (
             <li key={booking._id} className={styles.catalog_item}>
-              <p className={styles.catalog_item_title}>
-                Name:{" "}
-                <span className={styles.catalog_item_title_span}>
-                  {booking.clientName}
-                </span>
-              </p>
-              <p className={styles.catalog_item_title}>
-                Phone:{" "}
-                <span className={styles.catalog_item_title_span}>
-                  {booking.phoneNumber}
-                </span>
-              </p>
-              <p className={styles.catalog_item_title}>
-                Service:{" "}
-                <span className={styles.catalog_item_title_span}>
-                  {booking.serviceType}
-                </span>
-              </p>
-              {booking.dodatek !== "" && (
+              <div>
+                {" "}
                 <p className={styles.catalog_item_title}>
-                  Dodatek:{" "}
+                  Name:{" "}
                   <span className={styles.catalog_item_title_span}>
-                    {booking.dodatek}
+                    {booking.clientName}
                   </span>
                 </p>
-              )}
-              <p className={styles.catalog_item_title}>
-                Data:{" "}
-                <span className={styles.catalog_item_title_span}>
-                  {booking.date
-                    ? new Date(booking.date).toLocaleDateString()
-                    : "Brak daty"}
-                  ;
-                </span>
-              </p>
-              <p className={styles.catalog_item_title}>
-                Time:{" "}
-                <span className={styles.catalog_item_title_span}>
-                  {booking.time}
-                </span>
-              </p>
-              <button
-                className={styles.catalog_item_btn}
-                type="button"
-                onClick={() => handleDelet(booking._id)}
-              >
-                Delete
-              </button>
+                <p className={styles.catalog_item_title}>
+                  Phone:{" "}
+                  <span className={styles.catalog_item_title_span}>
+                    {booking.phoneNumber}
+                  </span>
+                </p>
+                <p className={styles.catalog_item_title}>
+                  Service:{" "}
+                  <span className={styles.catalog_item_title_span}>
+                    {booking.serviceType}
+                  </span>
+                </p>
+                {booking.dodatek !== "" && (
+                  <p className={styles.catalog_item_title}>
+                    Dodatek:{" "}
+                    <span className={styles.catalog_item_title_span}>
+                      {booking.dodatek}
+                    </span>
+                  </p>
+                )}
+                <p className={styles.catalog_item_title}>
+                  Data:{" "}
+                  <span className={styles.catalog_item_title_span}>
+                    {booking.date
+                      ? new Date(booking.date).toLocaleDateString()
+                      : "Brak daty"}
+                    ;
+                  </span>
+                </p>
+                <p className={styles.catalog_item_title}>
+                  Time:{" "}
+                  <span className={styles.catalog_item_title_span}>
+                    {booking.time}
+                  </span>
+                </p>
+              </div>
+
+              <div className={styles.catalog_item_btn_div}>
+                <button
+                  className={styles.catalog_item_btn}
+                  type="button"
+                  onClick={() => handleDelet(booking._id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className={styles.catalog_item_btn}
+                  type="button"
+                  onClick={() => handleDelet(booking._id)}
+                >
+                  Update
+                </button>
+              </div>
             </li>
           ))
         ) : (
